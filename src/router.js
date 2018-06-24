ad85ffab428e6723554686f6f1af404a50f413df
Cody <cody@dreamersofday.co>
2018-06-24 01:05:42 -0500

    add play view

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Play from './views/Play.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode  : 'history',
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path     : '/',
+      name     : 'home',
       component: Home,
+    },
+    {
+      path     : '/play',
+      name     : 'play',
+      component: Play,
     },
   ],
 });
---
+++ src/views/Play.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex w-screen h-screen bg-blue">
+    gameboard goes here
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'play',
+};
+</script>
