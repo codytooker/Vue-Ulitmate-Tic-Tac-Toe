a3877f9b4b90d3789ec42f6c8677ebbb7f12d985
Cody <cody@dreamersofday.co>
2018-06-23 23:58:36 -0500

    create button component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center justify-center w-screen h-screen">
-    <router-link class="border border-blue hover:text-white hover:bg-blue rounded text-blue py-3 px-3 no-underline" to="/play">Play Game</router-link>
+    <router-link class="btn btn--primary" to="/play">Play Game</router-link>
   </div>
 </template>
 
