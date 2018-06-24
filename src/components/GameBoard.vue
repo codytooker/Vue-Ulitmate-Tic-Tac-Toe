179be17421c1bf70d2ce4334dd820aa87c664eaa
Cody <cody@dreamersofday.co>
2018-06-24 17:15:32 -0500

    add localboard and use it in gameboard

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="flex flex-wrap bg-white w-700 h-700">
-    <div v-for="i in 9" :key="i">{{ i }}</div>
+    <div class="board-section flex-grow" v-for="i in 9" :key="i">
+      <local-board></local-board>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import LocalBoard from '@/components/LocalBoard.vue';
 
+export default {
+  components: {
+    LocalBoard,
+  },
 };
 </script>
-
-<style scoped>
-.game-board {
-  width: 700px;
-  height: 700px;
-}
-
-.game-board > div {
-  width: 33.333333333%;
-  border-bottom: 4px solid red;
-  border-right: 4px solid red;
-}
-
-.game-board > div:nth-last-child(-n+3) {
-  border-bottom: none;
-}
-
-.game-board > div:nth-child(3n) {
-  border-right: none;
-}
-</style>
---
+++ src/components/LocalBoard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-wrap w-full h-full">
+    <div class="board-section board-section--local" v-for="i in 9" :key="i"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'board-section',
+};
+</script>
