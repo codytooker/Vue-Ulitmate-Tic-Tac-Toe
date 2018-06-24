c119c17a4841189dbfd9f9aa1fca4b8fd0ca1091
Cody <cody@dreamersofday.co>
2018-06-24 11:54:15 -0500

    add to tailwind settings

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('postcss-import')(),
     require('postcss-nested')(),
-    require('tailwindcss')(),
+    require('tailwindcss')('./tailwind.js'),
     require('autoprefixer')(),
   ]
 }
