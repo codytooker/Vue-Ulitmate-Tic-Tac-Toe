734d40f704f08b05fb67177398de575fac8d6f84
Cody <cody@dreamersofday.co>
2018-06-24 01:36:44 -0500

    center everything in the play route

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex w-screen h-screen bg-blue">
+  <div class="flex items-center justify-center w-screen h-screen bg-blue">
     gameboard goes here
   </div>
 </template>
