80da6e099f4ce7a7f9b018889211e2246751fbaa
Cody <cody@dreamersofday.co>
2018-06-24 11:54:36 -0500

    add Gameboard to Play Component

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="flex items-center justify-center w-screen h-screen bg-blue">
-    gameboard goes here
+    <GameBoard></GameBoard>
   </div>
 </template>
 
 <script>
+import GameBoard from '@/components/GameBoard.vue';
+
 export default {
   name: 'play',
+  components: {
+    GameBoard,
+  },
 };
 </script>
