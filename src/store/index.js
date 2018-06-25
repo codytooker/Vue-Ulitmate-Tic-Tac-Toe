0503693ac69a8a3b9e722d11f7649f6f39e7ae91
Cody <cody@dreamersofday.co>
2018-06-25 17:45:34 -0500

    create localboard store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import localboard from '@/store/modules/localboard.js';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  modules: {
+    localboard,
+  }
 });
---
+++ src/store/modules/localboard.js
@@ -0,0 +1,39 @@
+const setInitialState = () => {
+  const state = [];
+  for (let i = 0; i < 9; i++) {
+    const el = {
+      winner: null,
+      squares: [],
+    };
+    for (let j = 0; j < 9; j++) {
+      const square = null;
+      el.squares.push(square);
+    }
+
+    state.push(el);
+  }
+
+  return state;
+}
+
+const squares = {
+  state: setInitialState,
+  getters: {
+    getBoardById: state => id => {
+      return state[id];
+    },
+    getSquareById: state => id => {
+      return state.find(square => square.id === id);
+    }
+  },
+  mutations: {
+    setSquareValue(state, { board, square, value }) {
+      if (state[board].squares[square] === null) {
+        console.log('updating');
+        state[board].squares.splice(square, 1, value);
+      }
+    }
+  }
+}
+
+export default squares;
