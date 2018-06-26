dddf9d4774da8bf7aab81d19c5aecedcf98e8263
Cody <cody@dreamersofday.co>
2018-06-25 22:14:55 -0500

    add conditional board winner div in localboard

--- src/components/LocalBoard.vue
+++ src/components/LocalBoard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="board board--local">
+    <div class="board__winner" v-if="localBoard.winner" v-text="localBoard.winner"></div>
     <cell v-for="(cell, index) in localBoard.cells"
             :key="index"
             :value="cell"
