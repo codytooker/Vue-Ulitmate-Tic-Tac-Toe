82508bcd59537325117693b2193c92ce6a369e9a
Cody <cody@dreamersofday.co>
2018-06-27 00:21:54 -0500

    set available boards after clicking a cell

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,7 @@ const getDefaultState = () => {
 
   for (let i = 0; i < 9; i += 1) {
     const el = {
+      isPlayable: true,
       winner: null,
       cells: [],
     };
@@ -36,12 +37,16 @@ export default new Vuex.Store({
   getters: {
     getBoardById: state => id => state.boards[id],
     getBoardWinners: state => state.boards.map(board => board.winner),
+    getBoardAvailability: state => state.boards.map(board => board.winner === null),
     getCurrentTurn: state => state.currentTurn,
     getWinner: state => state.winner,
   },
   mutations: {
     setCellValue(state, { board, cell, value }) {
       state.boards[board].cells.splice(cell, 1, value);
+    },
+    setBoardAvailability(state, { board, playable }) {
+      state.boards[board].isPlayable = playable;
     },
     setBoardWinner(state, { board, winner }) {
       state.boards[board].winner = winner;
@@ -57,11 +62,13 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    processTurn({ commit, getters }, { board, cell }) {
+    processTurn({ commit, dispatch, getters }, { board, cell }) {
+      console.log(cell);
       const localBoard = getters.getBoardById(board);
       const currentTurn = getters.getCurrentTurn;
 
-      if (localBoard.cells[cell] === null) {
+      if (localBoard.isPlayable && localBoard.cells[cell] === null) {
+        commit('changeCurrentTurn');
         commit('setCellValue', {
           board,
           cell,
@@ -80,16 +87,38 @@ export default new Vuex.Store({
             commit('setWinner', {
               symbol: currentTurn,
             });
+          } else {
+            dispatch('setAvailableBoards', cell);
           }
         } else {
-          console.log('no local winner yet');
+          dispatch('setAvailableBoards', cell);
         }
+      }
+    },
+    setAvailableBoards({ commit, getters }, id) {
+      const boardAvailability = getters.getBoardAvailability;
 
-
-        // if no winner set which local board is active
-
-        // if no winner change the current turn
-        commit('changeCurrentTurn');
+      if (boardAvailability[id]) {
+        boardAvailability.forEach((el, i) => {
+          if (i === id) {
+            commit('setBoardAvailability', {
+              board: i,
+              playable: true,
+            });
+          } else {
+            commit('setBoardAvailability', {
+              board: i,
+              playable: false,
+            });
+          }
+        });
+      } else {
+        boardAvailability.forEach((el, i) => {
+          commit('setBoardAvailability', {
+            board: i,
+            playable: true,
+          });
+        });
       }
     },
   },
