f302e546f19a402bf7997f618bd0085e0cefb303
Cody <cody@dreamersofday.co>
2018-06-27 11:53:16 -0500

    add purge css to postcss config

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,5 +4,9 @@ module.exports = {
     require('postcss-nested')(),
     require('tailwindcss')('./tailwind.js'),
     require('autoprefixer')(),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['**/*.vue'],
+      css: ['src/styles/main.css']
+    })
   ]
 }
